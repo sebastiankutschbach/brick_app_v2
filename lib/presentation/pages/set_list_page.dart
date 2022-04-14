196cc5206267617f10ce287c6a765d6a492aabc4
Sebastian Kutschbach <sebastian.kutschbach@freenet.de>
2022-04-14 10:25:20 +0200

    show set preview

--- lib/presentation/pages/set_list_page.dart
+++ lib/presentation/pages/set_list_page.dart
@@ -2,6 +2,7 @@ import 'package:brick_app_v2/application/cubit/set_list_page_cubit.dart';
 import 'package:brick_app_v2/domain/set.dart';
 import 'package:brick_app_v2/domain/set_list.dart';
 import 'package:brick_app_v2/injection.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -52,21 +53,40 @@ class SetListPage extends StatelessWidget {
   }
 
   _loadedBody(BuildContext context, SetListPageLoaded state) {
-    return Center(
-      child: ListView.builder(
-        itemBuilder: (context, index) =>
-            _buildSetTile(context, state.sets[index]),
-        itemCount: state.sets.length,
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Colors.grey[300]),
+      child: Center(
+        child: ListView.builder(
+          itemBuilder: (context, index) =>
+              _buildSetTile(context, state.sets[index]),
+          itemCount: state.sets.length,
+        ),
       ),
     );
   }
 
   _buildSetTile(BuildContext context, Set set) {
-    return ListTile(
-      key: Key('setTile-${set.setNum}'),
-      leading: const Icon(Icons.list),
-      title: Text(set.name),
-      subtitle: Text('${set.numParts} parts'),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        key: Key('setTile-${set.setNum}'),
+        child: Column(children: [
+          CachedNetworkImage(
+            imageUrl: set.setImgUrl.toString(),
+            placeholder: (context, url) => const CircularProgressIndicator(),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            height: 150,
+            width: 150,
+          ),
+          const SizedBox(height: 8.0),
+          Text(
+            set.name,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          Text('${set.numParts} parts'),
+          const SizedBox(height: 8.0),
+        ]),
+      ),
     );
   }
 }
