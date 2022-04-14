521614248f55f907dd9192186004f46557ae2716
Sebastian Kutschbach <sebastian.kutschbach@freenet.de>
2022-04-14 13:08:52 +0200

    added tests for moc list page cubit

---
+++ test/application/cubit/moc_list_page_cubit_test.dart
@@ -0,0 +1,53 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:brick_app_v2/application/cubit/moc_list_page_cubit.dart';
+import 'package:brick_app_v2/domain/failure.dart';
+import 'package:brick_app_v2/infrastructure/brick_set_repository.dart';
+import 'package:brick_app_v2/infrastructure/moc_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../domain/moc_test.dart';
+import '../../mocks.dart';
+
+main() {
+  const String setNum = '1';
+  const Failure failure = Failure('429');
+
+  final BrickSetRepository _brickSetRepository = MockBrickSetRepository();
+  final MocRepositoryFacade _mocRepository = MockMocRepository();
+
+  blocTest<MocListPageCubit, MocListPageState>(
+    'initial state is loading',
+    build: () => MocListPageCubit(_brickSetRepository, _mocRepository),
+    verify: (cubit) => expect(cubit.state is MocListPageLoading, true),
+  );
+
+  blocTest<MocListPageCubit, MocListPageState>(
+    'loading the set list emits first loading state and loaded state afterwards',
+    build: () {
+      when(() => _brickSetRepository.loadMocs(setNum: setNum))
+          .thenAnswer((_) async => right([testMoc]));
+      when(() => _mocRepository.areBuildInstructionsAvailable(
+          setNum: setNum,
+          mocNums: [testMoc.mocNum])).thenAnswer((_) async => [testMoc.mocNum]);
+      return MocListPageCubit(_brickSetRepository, _mocRepository);
+    },
+    act: (cubit) => cubit.loadMocList(setNum),
+    expect: () => [
+      MocListPageLoading(),
+      MocListPageLoaded([testMoc])
+    ],
+  );
+
+  blocTest<MocListPageCubit, MocListPageState>(
+    'loading the set list emits first loading state and error state afterwards if loading fails',
+    build: () {
+      when(() => _brickSetRepository.loadMocs(setNum: setNum))
+          .thenAnswer((_) async => left(failure));
+      return MocListPageCubit(_brickSetRepository, _mocRepository);
+    },
+    act: (cubit) => cubit.loadMocList(setNum),
+    expect: () => [MocListPageLoading(), const MocListPageError(failure)],
+  );
+}
--- test/mocks.dart
+++ test/mocks.dart
@@ -3,6 +3,8 @@ import 'package:brick_app_v2/application/cubit/authentication_cubit.dart';
 import 'package:brick_app_v2/application/cubit/home_page_cubit.dart';
 import 'package:brick_app_v2/application/cubit/set_list_page_cubit.dart';
 import 'package:brick_app_v2/application/cubit/settings_cubit.dart';
+import 'package:brick_app_v2/infrastructure/brick_set_repository.dart';
+import 'package:brick_app_v2/infrastructure/moc_repository.dart';
 import 'package:brick_app_v2/infrastructure/set_list_repository.dart';
 import 'package:brick_app_v2/infrastructure/user_token_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -23,7 +25,11 @@ class MockSetListPageCubit extends MockCubit<SetListPageState>
 
 class MockSetListRepository extends Mock implements SetListRepository {}
 
+class MockBrickSetRepository extends Mock implements BrickSetRepository {}
+
 class MockUserTokenRepository extends Mock implements UserTokenRepository {}
+
+class MockMocRepository extends Mock implements MocRepositoryFacade {}
 
 class MockStorage extends Mock implements Storage {}
 
